8b7694300212c4556d599187377e63b19f5ecc04
Veeresh1012 <veereshangadi1012@gmail.com>
2026-08-19 13:08:05 +0000

    Add addition Bash script

---
+++ add.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter the first number:"
+read first_number
+
+echo "Enter the second number:"
+read second_number
+
+result=$((first_number + second_number))
+
+echo "Addition result: $first_number + $second_number = $result"
